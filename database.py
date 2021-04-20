a6e7c025b5e0703a865ab4bbac20b5161594e685
ryorichardo <37143513+ryorichardo@users.noreply.github.com>
2021-04-20 14:19:59 +0700

    method2 db udh semua (hrsnya)

--- database.py
+++ database.py
@@ -5,15 +5,15 @@ import datetime
 def connect():
     path = str(pathlib.Path(__file__).parent.absolute())
     return sqlite3.connect(path + "/database.db")
-
     # cara pake di main: connection = database.connect()
 
-def create_tables(connection):
+def createTables(connection):
     # id | date | matkul | jenis | deskripsi | 
     with connection:
         connection.execute("CREATE TABLE IF NOT EXISTS task (id INTEGER PRIMARY KEY AUTOINCREMENT, deadline DATE, matkul TEXT, jenis TEXT, deskripsi TEXT)")
 
-def add_task(connection, deadline, matkul, jenis, deskripsi):
+def addTask(connection, deadline, matkul, jenis, deskripsi):
+    # Poin 1
     # asumsi: deadline adalah string formatnya dd/mm/yyyy atau dd-mm-yyyy
     day = int(deadline[0:2])
     month = int(deadline[3:5])
@@ -26,13 +26,15 @@ def add_task(connection, deadline, matkul, jenis, deskripsi):
             VALUES(?,?,?,?)
         ''', (tanggal, matkul, jenis, deskripsi))
 
-def get_all_task(connection):
+def getTaskAll(connection):
+    # Poin 2a
     with connection:
         return connection.execute('''
             SELECT * FROM task
         ''').fetchall()
 
-def get_perioded_task(connection, date1, date2):
+def getTaskByPeriod(connection, date1, date2):
+    # Poin 2b
     day1 = int(date1[0:2])
     month1 = int(date1[3:5])
     year1 = int(date1[6:10]) 
@@ -45,10 +47,40 @@ def get_perioded_task(connection, date1, date2):
     with connection:
         return connection.execute('''
             SELECT * FROM task
-            WHERE 
-        ''')
+            WHERE deadline >= ? AND deadline <= ?
+        ''', (tanggal1, tanggal2))
 
-def delete_task(connection, id):
+def getTaskByType(connection, jenis):
+    # Poin 2c
+    with connection:
+        return connection.execute('''
+            SELECT * FROM task
+            WHERE jenis = ?
+        ''', (jenis,))
+
+def getTaskByMatkulType(connection, matkul, jenis):
+    # Poin 3
+    with connection:
+        return connection.execute('''
+            SELECT * FROM task
+            WHERE matkul = ? AND jenis = ?
+        ''', (matkul, jenis))
+
+def updateTaskDeadline(connection, id, deadline):
+    # Poin 4
+    day = int(deadline[0:2])
+    month = int(deadline[3:5])
+    year = int(deadline[6:10]) 
+    tanggal = datetime.date(year, month, day)
+    with connection:
+        connection.execute('''
+            UPDATE task
+            SET deadline = ?
+            WHERE id = ?
+        ''', (tanggal, id))
+
+def deleteTask(connection, id):
+    # Poin 5
     with connection:
         connection.execute('''
             DELETE FROM task
