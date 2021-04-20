e7e868e0c6ed03a766ecc08b9fb3339a46ef27a4
darubagus <darubagus@gmail.com>
2021-04-20 12:23:08 +0700

    yea

---
+++ app.py
@@ -0,0 +1,27 @@
+import os
+from flask import *
+import sqlite3 as sql
+
+app = Flask(__name__)
+
+# APP_ROUTE = os.path.dirname(os.path.abspath(__file__))
+
+kataPenting = [
+    'tucil' , 
+    'tubes', 
+    'kuis',
+    'ujian',
+    'praktikum',
+    'mummu'
+]
+
+@app.route("/")
+def index():
+    return render_template("search.html")
+
+
+
+
+# MAIN
+if __name__ == "__main__":
+    app.run(debug=True)
